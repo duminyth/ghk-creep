1f5fd471f89e0def850a4a5108c66696af9743df
duminyth <duminyth@gmail.com.com>
2026-06-18 15:49:52 +0200

    Divers modification

--- inp_unitcell_2d.py
+++ inp_unitcell_2d.py
@@ -193,13 +193,48 @@ def Plot_Pq(p,q,titel):
 	fig, ax = plt.subplots(figsize=(8*cm, 7*cm))
 	maxX = np.max(p)
 	maxq = np.max(q)
+
+	ax.plot((0,0),(0,maxq*1.1),'-k')
+	ax.plot((0,0),(0,maxX*1.1),'-k')
+	ax.plot((maxq/np.tan(np.radians(71.57)),maxq),(0,0),'-k')
+	
+	if False:
+		points = [[0,0], [maxq/np.tan(np.radians(71.57)),maxq], [0, maxq]]
+		triangle = Polygon(points, fc=(1, 0, 0, 0.5), ec='red', lw=0)
+		ax.add_patch(triangle)
+		
+		points = [[0,0], [maxq/np.tan(np.radians(71.57)),maxq], [6,0]]
+		triangle2 = Polygon(points, fc=(0, 0, 1, 0.5), ec='blue', lw=0)
+		ax.add_patch(triangle2)
+		points = [[maxX,maxq], [maxq/np.tan(np.radians(71.57)),maxq], [6,0]]
+		triangle2 = Polygon(points, fc=(0, 0, 1, 0.5), ec='blue', lw=0)
+		ax.add_patch(triangle2)
+		
+		
+	plt.plot(p,q, 'ob')
+	ax.set_aspect('equal', adjustable='box')
+	ax.set_xlabel("p (MPa)")
+	ax.set_ylabel("q (MPa)")
     
 	ax.plot((0,0),(0,maxq*1.1),'-k')
 	ax.plot((0,0),(0,maxX*1.1),'-k')
 	ax.plot((maxq/np.tan(np.radians(71.57)),maxq),(0,0),'-k')
     
-	ax.text(0.75*maxX,0.75*maxq, 'Multi axial compression')
-	ax.text(0.1,maxq*1.2, 'Shear-compression')
+	plt.gca().set_frame_on(False)
+	plt.savefig(path+str(titel)+'.png', dpi=300)
+    
+	
+	plt.close(fig)
+
+	cm = 1 / 2.54
+	fig, ax = plt.subplots(figsize=(8*cm, 7*cm))
+	maxX = np.max(p)
+	maxq = np.max(q)
+    
+	ax.plot((0,0),(0,maxq*1.1),'-k')
+	ax.plot((0,0),(0,maxX*1.1),'-k')
+	ax.plot((maxq/np.tan(np.radians(71.57)),maxq),(0,0),'-k')
+    
     
 	points = [[0,0], [maxq/np.tan(np.radians(71.57)),maxq], [0, maxq]]
 	triangle = Polygon(points, fc=(1, 0, 0, 0.5), ec='red', lw=0)
@@ -219,14 +254,15 @@ def Plot_Pq(p,q,titel):
 	ax.set_aspect('equal', adjustable='box')
 	ax.set_xlabel("p (MPa)")
 	ax.set_ylabel("q (MPa)")
-	plt.title(titel)
     
 	ax.plot((0,0),(0,maxq*1.1),'-k')
 	ax.plot((0,0),(0,maxX*1.1),'-k')
 	ax.plot((maxq/np.tan(np.radians(71.57)),maxq),(0,0),'-k')
+	ax.set_ylim(5,15)
+	ax.set_xlim(2.5,9)
     
 	plt.gca().set_frame_on(False)
-	plt.savefig(path+'pq_dt'+str(dt)+'.png', dpi=300)
+	plt.savefig(path+str(titel)+'_zoomed.png', dpi=300)
     
 	
 	plt.close(fig)
@@ -237,8 +273,8 @@ def Plot_eps(e_pl,e_cr,titel):
     
 	cm = 1 / 2.54
 	fig, ax = plt.subplots(figsize=(8*cm, 7*cm))
-
     
+        
 	plt.plot(e_pl,e_cr, 'ob')
 	ax.set_xlabel(r"$\varepsilon_\mathrm{pl}$")
 	ax.set_ylabel(r"$\varepsilon_\mathrm{cr}$")
@@ -248,15 +284,15 @@ def Plot_eps(e_pl,e_cr,titel):
 	plt.gca().set_frame_on(True)
 	plt.gca().ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
 	plt.tight_layout()
-
-
-
+    
+    
+    
 	plt.savefig(path+'eps_dt'+str(dt)+'.png', dpi=300, bbox_inches="tight")
 	
 	plt.close(fig)
 	return
 
-def Plot_check_0(t,eps_pl, eps_cr, q, p, Deps_cr):
+def Plot_check_0(t,eps_pl, eps_cr, q, p, Deps_cr, titel):
 	cm = 1 / 2.54
 	fig, axes = plt.subplots(
 		4, 1,
@@ -281,7 +317,7 @@ def Plot_check_0(t,eps_pl, eps_cr, q, p, Deps_cr):
 		ax.grid(True)
     
 	plt.tight_layout()
-	plt.savefig(path+'Evolution_qty'+str(dt)+'.png', dpi=300)
+	plt.savefig(path+'Evolution_qty'+str(dt)+'_'+str(titel)+'.png', dpi=300)
     
 	
 	plt.close(fig)
@@ -291,31 +327,29 @@ def Plot_check_0(t,eps_pl, eps_cr, q, p, Deps_cr):
 	ax.plot(t, Deps_cr)
 	ax.set_ylabel(r"$\Delta\varepsilon_\mathrm{cr}$")
 	ax.set_xlabel("$time(s)$")
-
+    
 	ax.set_aspect('auto', adjustable='box')
 	plt.gca().set_frame_on(True)
 	plt.tight_layout()
-	plt.savefig(path+'DelatCreep'+str(dt)+'.png', dpi=300,bbox_inches="tight")
+	plt.savefig(path+'DelatCreep'+str(dt)+'_'+str(titel)+'.png', dpi=300,bbox_inches="tight")
 	
 	plt.close(fig)
-
-
-
+    
+    
+    
 	cm = 1 / 2.54
 	fig, ax = plt.subplots(figsize=(8*cm, 7*cm))
 	ax.plot(t, eps_cr)
 	ax.set_ylabel(r"$\varepsilon_\mathrm{cr}$")
 	ax.set_xlabel("$time(s)$")
-
+    
 	ax.set_aspect('auto', adjustable='box')
 	plt.gca().set_frame_on(True)
 	plt.tight_layout()
 	plt.gca().ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
-	plt.savefig(path+'Eps_Creep'+str(dt)+'.png', dpi=300,bbox_inches="tight")
+	plt.savefig(path+'Eps_Creep'+str(dt)+'_'+str(titel)+'.png', dpi=300,bbox_inches="tight")
 	
 	plt.close(fig)
-
-
     
 	return
 
@@ -363,11 +397,159 @@ def Plot_stress_all(results):
     
 	return
 
+def Extract_pq_ABQ_noUMAT(odb, path):
+	odbNew = str(path+odb[:-4])+'.odb'
+	odb_object = openOdb(odbNew)
+	session.viewports['Viewport: 1'].setValues(displayedObject=odb_object)
+	V = (('S', INTEGRATION_POINT),)
+	
+	xyd = session.xyDataListFromField(odb = odb_object, outputPosition=ELEMENT_NODAL, variable = V, nodeSets = ('RBM',))
+    
+
+	VAR_S11   = np.array([[y for x,y in XY] for XY in xyd if 'S11' in XY.name])[0]
+	VAR_S22   = np.array([[y for x,y in XY] for XY in xyd if 'S22' in XY.name])[0]
+	VAR_S33   = np.array([[y for x,y in XY] for XY in xyd if 'S33' in XY.name])[0]
+	VAR_S12   = np.array([[y for x,y in XY] for XY in xyd if 'S12' in XY.name])[0]
+	t   = np.array([[x for x,y in XY] for XY in xyd if 'S12' in XY.name])[0]
+
+
+	VAR_S13   = 0.
+	VAR_S23   = 0.
+    
+	p = (VAR_S11+VAR_S22+VAR_S33)*1/3.
+    
+	q = np.sqrt(3/2)*np.sqrt(VAR_S11**2+VAR_S22**2+VAR_S12**2+VAR_S13**2+VAR_S23**2)
+    
+	return p,q,t
+
+
+def Extract_Strain_ABQ_noUMAT(odb, path):
+	odbNew = str(path+odb[:-4])+'.odb'
+	odb_object = openOdb(odbNew)
+	session.viewports['Viewport: 1'].setValues(displayedObject=odb_object)
+	V = (('CEEQ', INTEGRATION_POINT),)
+	
+	xyd = session.xyDataListFromField(odb = odb_object, outputPosition=ELEMENT_NODAL, variable = V, nodeSets = ('RBM',))
+    
+
+	VAR_CEEQ   = np.array([[y for x,y in XY] for XY in xyd if 'CEEQ' in XY.name])[0]
+
+    
+	return VAR_CEEQ
+
+
+def Extract_Strain_ABQ_CreepRoutine(odb, path):
+	odbNew = str(path+odb[:-4])+'.odb'
+	odb_object = openOdb(odbNew)
+	session.viewports['Viewport: 1'].setValues(displayedObject=odb_object)
+	V = (('SDV1', INTEGRATION_POINT),)
+	
+	xyd = session.xyDataListFromField(odb = odb_object, outputPosition=ELEMENT_NODAL, variable = V, nodeSets = ('RBM',))
+    
+
+	VAR_CEEQ   	= [[y for x, y in XY] for XY in xyd if 'SDV1' in XY.name]
+	VAR_CEEQ   	= np.array([[y for x,y in XY] for XY in xyd if 'SDV1' in XY.name])[0]
+	t   		= [[x for x, y in XY] for XY in xyd if 'SDV1' in XY.name][0]
+
+	cm = 1 / 2.54
+	fig, ax = plt.subplots(figsize=(8*cm, 4*cm))
+	ax.plot(t, VAR_CEEQ)
+	ax.set_ylabel(r"$\varepsilon_\mathrm{cr}$")
+	ax.set_xlabel("$time(s)$")
+    
+	ax.set_aspect('auto', adjustable='box')
+	plt.gca().set_frame_on(True)
+	plt.tight_layout()
+	plt.gca().ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+	plt.savefig(path+'Eps_Creep'+str(dt)+'.png', dpi=300,bbox_inches="tight")
+	
+	plt.close(fig)
+    
+	return VAR_CEEQ
+
+
+def Extract_Strain_ABQ_Nothing(odb, path, name):
+	odbNew = str(path+odb[:-4])+'.odb'
+	odb_object = openOdb(odbNew)
+	session.viewports['Viewport: 1'].setValues(displayedObject=odb_object)
+	V = (('CEEQ', INTEGRATION_POINT),('PEEQ', INTEGRATION_POINT),('EE', INTEGRATION_POINT),('CE', INTEGRATION_POINT),('PE', INTEGRATION_POINT),)
+	
+	xyd = session.xyDataListFromField(odb = odb_object, outputPosition=ELEMENT_NODAL, variable = V, nodeSets = ('RBM',))
+    
+
+	VAR_CEEQ   	= np.array([[y for x,y in XY] for XY in xyd if 'CEEQ' in XY.name])[0]
+	VAR_PEEQ   	= np.array([[y for x,y in XY] for XY in xyd if 'PEEQ' in XY.name])[0]
+	VAR_EE22   	= np.array([[y for x,y in XY] for XY in xyd if 'EE22' in XY.name])[0]
+	VAR_CE22   	= np.array([[y for x,y in XY] for XY in xyd if 'CE22' in XY.name])[0]
+	VAR_PE22   	= np.array([[y for x,y in XY] for XY in xyd if 'PE22' in XY.name])[0]
+	t   		= [[x for x, y in XY] for XY in xyd if 'CEEQ' in XY.name][0]
+
+	cm = 1 / 2.54
+	fig, ax = plt.subplots(figsize=(8*cm, 4*cm))
+	ax.plot(t, VAR_CEEQ)
+	ax.set_ylabel(r"$\varepsilon_\mathrm{cr}$")
+	ax.set_xlabel("$time(s)$")
+    
+	ax.set_aspect('auto', adjustable='box')
+	plt.gca().set_frame_on(True)
+	plt.tight_layout()
+	plt.gca().ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+	plt.savefig(path+'Eps_Creep'+str(dt)+'_'+str(name)+'.png', dpi=300,bbox_inches="tight")
+	
+	plt.close(fig)
+
+	cm = 1 / 2.54
+	fig, ax = plt.subplots(figsize=(8*cm, 4*cm))
+	ax.plot(t, VAR_PEEQ)
+	ax.set_ylabel(r"$\varepsilon_\mathrm{pl}$")
+	ax.set_xlabel("$time(s)$")
+    
+	ax.set_aspect('auto', adjustable='box')
+	plt.gca().set_frame_on(True)
+	plt.tight_layout()
+	plt.gca().ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+	plt.savefig(path+'Eps_Plas'+str(dt)+'_'+str(name)+'.png', dpi=300,bbox_inches="tight")
+	
+	plt.close(fig)
+    
+	return VAR_CEEQ,VAR_PEEQ, VAR_EE22, VAR_CE22, VAR_PE22
+
+
+def Plot_stress_strain(p,q,ce,pe,ee, titel):
+    
+	cm = 1 / 2.54
+	fig, ax = plt.subplots(figsize=(8*cm, 7*cm))
+	maxX = np.max(p)
+	maxq = np.max(q)
+    
+	total_strain = np.abs(ce+pe+ee)
+    
+	plt.plot(total_strain,q, 'ob', label='q')
+	plt.plot(total_strain,p, '+r', label='p')
+	ax.set_aspect('equal', adjustable='box')
+	ax.set_xlabel(r"$\varepsilon_\mathrm{pl}$")
+	ax.set_ylabel(r"$\sigma$")
+	ax.legend()
+
+	ax.set_aspect('auto', adjustable='box')
+	plt.gca().set_frame_on(True)
+	plt.tight_layout()
+	plt.savefig(path+str(titel)+'.png', dpi=300)
+    
+	
+	plt.close(fig)
+
+	with open(path+'stress_strain_data_'+str(titel)+'.dat', 'w') as fid:
+		for x,y,z in zip(total_strain,p,q):
+			fid.write(str(x)+'\t'+str(y)+'\t'+str(z)+'\n')
+	return
 
 
 path = 'G:/01_Forschung/01_Creep/00_UMAT/UMAT_CLAUDE/UnitCell_2D/time_increment_umat_v2/1E-13/'
-path = 'G:/01_Forschung/01_Creep/00_UMAT/UMAT_CLAUDE/UnitCell_2D/time_increment_umat_v2/Effect_decr_sub/K=2000/'
-path = 'G:/01_Forschung/01_Creep/00_UMAT/UMAT_CLAUDE/UnitCell_2D/time_increment_umat_v2/Effect_ecr0/K_1000/'
+path = 'G:/01_Forschung/01_Creep/00_UMAT/UMAT_CLAUDE/UnitCell_2D/time_increment_umat_v2/Effect_decr_sub/K=1000/'
+#path = 'G:/01_Forschung/01_Creep/00_UMAT/UMAT_CLAUDE/UnitCell_2D/time_increment_umat_v2/Effect_ecr0/K_1000/'
+path = 'G:/01_Forschung/01_Creep/00_UMAT/UMAT_CLAUDE/Comparing_Method/NewUMAT/'
+
 try:
 	os.mkdir(path)
 except WindowsError:
@@ -376,41 +558,67 @@ except WindowsError:
 os.chdir(path)
 delta_t = [1,5,25,150,500,750]
 delta_t = [2,6,10,12,15]
-delta_t = [12]
+delta_t = [4,10,12,14]
 p_all = []
 q_all = []
 if True:
-	for dt in delta_t[:1]:
-		#ABQ_model(dt)
-		
-		odb = 'E-'+str(dt)+'.odb'
-		#odb = 'Creep_'+str(dt)+'.odb'
-		t,sd1,sd2,sd3,sd4,sd5,LE = PostProcess(odb)
-		
-		with open('Result_'+odb[:-4]+'.dat','w') as fid:
-			for tti, s1,s2,s3,s4,s5 in zip(t[0],sd1[0],sd2[0],sd3[0],sd4[0], sd5[0]):
-				fid.write(str(tti)+"\t"+str(s1)+"\t"+str(s2)+'\t'+str(s3)+'\t'+str(-s4)+'\t'+str(s5)+'\n')
-		
-		p_all.append((t[0], sd3[0],sd4[0], dt,LE[0],sd1[0],sd2[0]))
-		
-		
-		Plot_Pq(-np.array(sd4[0]),sd3[0], 'dt='+str(dt))
-		Plot_eps(sd1[0],sd2[0], "")
-		Plot_check_0(t[0],sd1[0],sd2[0],sd3[0],sd4[0], sd5[0])
+	for dt in delta_t[1:2]:
 
-	Plot_stress_all(p_all)
+		if True:
+			#New UMAT
+				#ABQ_model(dt)
+				
+				odb = 'E-12.odb'
+				odb = 'k_2_5_3p.odb'
+				#odb = 'Creep_'+str(dt)+'.odb'
+				t,sd1,sd2,sd3,sd4,sd5,LE = PostProcess(odb)
+				
+				with open('Result_'+odb[:-4]+'.dat','w') as fid:
+					for tti, s1,s2,s3,s4,s5 in zip(t[0],sd1[0],sd2[0],sd3[0],sd4[0], sd5[0]):
+						fid.write(str(tti)+"\t"+str(s1)+"\t"+str(s2)+'\t'+str(s3)+'\t'+str(-s4)+'\t'+str(s5)+'\n')
+				
+				p_all.append((t[0], sd3[0],sd4[0], dt,LE[0],sd1[0],sd2[0]))
+				
+				
+				Plot_Pq(-np.array(sd4[0]),sd3[0], '3p')
+				Plot_eps(sd1[0],sd2[0], "")
+				Plot_check_0(t[0],sd1[0],sd2[0],sd3[0],sd4[0], sd5[0], '3p')
+	if False:
+		Plot_stress_all(p_all)
+    
+    
+	# ABQ model 
+
+	if False:
+		odb = 'Creep_Abq_0_5_3P.odb'
+		path = 'G:/01_Forschung/01_Creep/00_UMAT/UMAT_CLAUDE/Comparing_Method/UnitCell_Comparison_Jin/'
+		p,q,t = Extract_pq_ABQ_noUMAT(odb, path )
+		ceeq, peeq, ee,ce,pe = Extract_Strain_ABQ_Nothing(odb, path, '3_P')
+		Plot_Pq(-np.array(p),q, 'alpha_0_5_3P')
+		Plot_stress_strain(-np.array(p),q,ce,pe,ee,'stress_strain_curve')
+
+
+	# Jin routine model 
+
+	if False:
+		odb  = 'E-12.odb'
+		path = 'G:/01_Forschung/01_Creep/00_UMAT/UMAT_CLAUDE/Comparing_Method/Comparison_Shengli/'
+		p,q,t  = Extract_pq_ABQ_noUMAT(odb, path )
+		ceeq = Extract_Strain_ABQ_CreepRoutine(odb, path)
+		Plot_Pq(-np.array(p),q, 'dt='+str(dt))
 
 
 
 
 
 
-delta_t = np.array([2,6,8,12,15,1e100])
+
+delta_t = np.array([1,5,6,7,10,11,12,13,14])
 dtt = 10.**-delta_t
 
-if True:
+if False:
 
-	CPU_time = np.array([6.79e2,6.78e2,6.69e2,6.67e2,7.82e2,7.25e2])/60.
+	CPU_time = np.array([703,687,707,735,712,741,853,1620,8640])/60.
 
 	cm = 1 / 2.54
 	fig, axes = plt.subplots(figsize=(12*cm, 6*cm))
@@ -422,8 +630,8 @@ if True:
 
 	axes.set_xlabel(r"$\varepsilon_\mathrm{sub}$")
 	axes.set_xlabel(r"$\varepsilon_0^\mathrm{cr}$")
-	axes.set_yticks((10,15))
-	axes.set_xlim(-1,2*10**-2)
+	axes.set_yticks((10,100))
+	axes.set_xlim(1e-15,2*10**-1)
 
 	plt.tight_layout()
 
